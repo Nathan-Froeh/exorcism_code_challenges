35e5acb0fb87ad326658563e1565548a1db522c5
Nathan <nathan.froeh@gmail.com>
2019-09-24 14:49:19 -0600

    Pass resistor-color

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -3,8 +3,16 @@
 // convenience to get you started writing code faster.
 //
 
-export const colorCode = () => {
-  throw new Error("Remove this statement and implement this function");
+export const colorCode = (color) => {
+  const value = COLORS.reduce((acc, cur, i) => {
+    if(color === cur) {
+      acc = i
+    }
+    return acc
+  }, 0)
+  return value
 };
 
-export const COLORS = undefined;
+export const COLORS = [
+  "black","brown","red","orange",
+  "yellow","green","blue","violet","grey","white"]
