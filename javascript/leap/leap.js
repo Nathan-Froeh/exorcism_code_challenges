486c11d20859be334248e77d7ef4aa22af3bb529
Nathan <nathan.froeh@gmail.com>
2019-09-25 10:04:06 -0600

    Add year divisible by 100 and not 400

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -4,5 +4,11 @@
 //
 
 export const isLeap = (year) => {
-  return year % 4 === 0
+  if(year % 4 === 0 && year % 100 !== 0) {
+    return true
+  } else if (year % 100 !== 0 && year % 400 === 0) {
+    return false
+  } else {
+    return false
+  }
 };
--- javascript/leap/leap.spec.js
+++ javascript/leap/leap.spec.js
@@ -5,11 +5,11 @@ describe('A leap year', () => {
     expect(isLeap(2015)).toBe(false);
   });
 
-  xtest('year divisible by 4, not divisible by 100: leap year', () => {
+  test('year divisible by 4, not divisible by 100: leap year', () => {
     expect(isLeap(2016)).toBe(true);
   });
 
-  xtest('year divisible by 100, not divisible by 400: common year', () => {
+  test('year divisible by 100, not divisible by 400: common year', () => {
     expect(isLeap(2100)).toBe(false);
   });
 
