7ae58ac17e7edc08aaecb949274fc3a53c09f0c0
Nathan <nathan.froeh@gmail.com>
2019-09-26 14:15:43 -0600

    Pass gigasecond

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,6 +3,9 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error("Remove this statement and implement this function");
+export const gigasecond = (time) => {
+  const gig = 1000000000
+  const seconds = time.getTime() / 1000
+  const gigTime = seconds + gig
+  return new Date(gigTime * 1000)
 };
