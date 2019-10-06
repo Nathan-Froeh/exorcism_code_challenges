3c43365037a91d8d1c9cf609dbaf568aec59e0f6
Nathan <nathan.froeh@gmail.com>
2019-10-06 13:16:50 -0600

    Set up for recursion

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,8 +3,15 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = (num) => {
+export const steps = (num, step = 0) => {
+
   if(num < 1) {
     throw new Error('Only positive numbers are allowed')
+  } else if(num === 1) {
+    return step
+  } else if(num % 2 === 0) {
+    //If n is even, divide n by 2 to get n / 2.
+  } else if(num % 1 === 0) {
+    //If n is odd, multiply n by 3 and add 1 to get 3n + 1.
   }
 };
