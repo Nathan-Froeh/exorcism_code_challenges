3007a8174def400787e813f26f7d653d44757dd1
Nathan <nathan.froeh@gmail.com>
2019-10-06 13:11:06 -0600

    Add collatz-conjecture

---
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const steps = (num) => {
+  if(num < 1) {
+    throw new Error('Only positive numbers are allowed')
+  }
+};
