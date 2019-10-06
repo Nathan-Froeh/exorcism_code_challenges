7b8e27631996e0ec5f3c26000aedea39a62d2949
Nathan <nathan.froeh@gmail.com>
2019-10-06 13:20:00 -0600

    Pass all with recursion

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -10,8 +10,9 @@ export const steps = (num, step = 0) => {
   } else if(num === 1) {
     return step
   } else if(num % 2 === 0) {
-    //If n is even, divide n by 2 to get n / 2.
+    num = num / 2
   } else if(num % 1 === 0) {
-    //If n is odd, multiply n by 3 and add 1 to get 3n + 1.
+    num = num * 3 + 1
   }
+  return steps(num, step + 1)
 };
